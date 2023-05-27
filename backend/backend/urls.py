d6cf1f57dd8bdedd7086a721b5b9d5054046bf0b
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-27 14:09:24 +0000

    [backend] setup routes to render uploaded media

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("events.urls")),
-]
+    path('api-auth/', include(DefaultRouter().urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
