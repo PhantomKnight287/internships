9c857a22ad096b41e66f4a64e6348e29b843e073
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-27 14:09:53 +0000

    [backend] create new urls

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+# register the urls for the events app
 urlpatterns = [
-    path('',views.get_events),
-    path('login/',views.login),
-    path('register/',views.register)
+    path('auth/login',views.login),
+    path('auth/register',views.register),
+    path('auth/me',views.get_profile),
+    path('events',views.get_events),
+    path('events/me',views.my_events),
+    path('events/<int:event_id>/like',views.like_event),
+    path('events/<int:event_id>/unlike',views.unlike_event),
+    path('event/<int:event_id>',views.get_event),
+    path("event",views.create_event)
 ]
