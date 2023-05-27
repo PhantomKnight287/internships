0efbab0b8e788212c81f3794fac94b004428ab21
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-27 14:10:52 +0000

    [frontend] add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,30 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/header";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Header from "./components/shared/header";
 import Register from "./routes/auth/register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Login from "./routes/auth/login";
+import Events from "./routes/events";
+import EventInfo from "./routes/event";
+import CreateEvent from "./routes/events/create";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ToastContainer />
         <Header />
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route
-            path="/auth/login"
-            element={
-              <div>
-                <h1>Login</h1>
-              </div>
-            }
-          />
+          <Route path="/" element={<Navigate to="/events" replace />} />
+          <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/event/:id" element={<EventInfo />} />
+          <Route path="/events/create" element={<CreateEvent />} />
         </Routes>
       </BrowserRouter>
     </>
