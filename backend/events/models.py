28e0d2cac50112e58e24e4a18349fcff539d657d
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-27 14:09:33 +0000

    [backend] update models

--- backend/events/models.py
+++ backend/events/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    events = models.ManyToManyField('Event', related_name='events', blank=True)
+    liked_events = models.ManyToManyField('Event', related_name='liked_events', blank=True)
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
     data = models.TextField()
     time = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
     location = models.TextField()
     image = models.ImageField(upload_to='images/', null=True, blank=True)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    is_liked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.name} - {self.author}"
