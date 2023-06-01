43e405a747e725679b0676820d12018c6119f045
Gurpal Singh <gillgurpal699@gmail.com>
2023-06-01 22:13:59 +0530

    [mobile] create `MusicSource` model

---
+++ application/lib/models/music.dart
@@ -0,0 +1,15 @@
+class MusicSource {
+  final String name;
+  final String fileName;
+  final String artwork;
+  final String artist;
+  final String attribution;
+
+  const MusicSource({
+    required this.name,
+    required this.fileName,
+    required this.artwork,
+    required this.artist,
+    required this.attribution,
+  });
+}
