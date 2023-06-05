5c2d814f0ad732eaa0ee0ff778b78ee001b93df4
Gurpal Singh <gillgurpal699@gmail.com>
2023-06-05 15:39:31 +0530

    [frontend] setup `react-router-dom`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/shared/header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import { useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "./utils/firebase";
+import { useUserStore } from "./context/user";
+import RootRoute from "./routes";
+import Login from "./routes/login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const setUser = useUserStore((s) => s.setUser);
+  useEffect(() => {
+    const unsub = getAuth(app).onAuthStateChanged((data) => {
+      if (!data) {
+        setUser(null);
+        return;
+      }
+      setUser({
+        displayName: data.displayName,
+        email: data.email,
+        photoURL: data.photoURL,
+        uid: data.uid,
+      });
+    });
+    return () => unsub();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <ToastContainer />
+        <Header />
+        <Routes>
+          <Route path="/" element={<RootRoute />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
