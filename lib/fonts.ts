60485eb53fb0d456e477433784e0db30fe47d506
Gurpal Singh <gillgurpal699@gmail.com>
2023-06-18 18:43:56 +0530

    add Outfit font

---
+++ lib/fonts.ts
@@ -0,0 +1,7 @@
+import { Outfit } from "next/font/google";
+
+export const outfit = Outfit({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+});
