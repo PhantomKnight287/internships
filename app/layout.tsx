ddd5130e01508cf2ba6a2ca552b06379846b1f51
Gurpal Singh <gillgurpal699@gmail.com>
2023-06-19 10:50:09 +0530

    add padding to body in smaller screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
+import Header from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
@@ -33,7 +33,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
@@ -42,8 +41,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
+              <Header />
+              <div className="flex-1 p-4 lg:p-0">{children}</div>
             </div>
             <TailwindIndicator />
           </ThemeProvider>
